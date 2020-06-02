2a8a2325cd2b7da530bf8cb4c913613b5fb64dbc
Ryan Dick <rdick@ualberta.ca>
2020-06-01 18:43:31 -0700

    added location tracker to worker page - allows to track worker location

--- native-client/screens/Worker/WorkerScreen.js
+++ native-client/screens/Worker/WorkerScreen.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import * as Location from "expo-location";
 
 const WorkerScreen = props => {
+  const [location, setlocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(
+    function () {
+      if (location) {
+        fetch("https://location-app-5d3d8.firebaseio.com/locations.json", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            latitude: `${location.coords.latitude}`,
+            longitude: `${location.coords.longitude}`
+          })
+        })
+          .then(e => e.json())
+          .then(data => console.log(data));
+      }
+    },
+    [location]
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      (async () => {
+        let { status } = await Location.requestPermissionsAsync();
+        if (status !== "granted") {
+          setErrorMsg("Permission to access location was denied");
+        }
+        let location = await Location.getCurrentPositionAsync({});
+        setlocation(location);
+      })();
+    }, 9000);
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <View>
       <Text>WorkerScreen</Text>
