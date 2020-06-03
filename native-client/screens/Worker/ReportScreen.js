c258789a17109dddf7de2996e047ce9346ed3622
Akshay Nanda <akshayn@sfu.ca>
2020-06-02 20:34:47 -0700

    Image Picker ready: saving imaging in cache + Title in state

--- native-client/screens/Worker/ReportScreen.js
+++ native-client/screens/Worker/ReportScreen.js
@@ -1,20 +1,72 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet
+} from "react-native";
+// import { useDispatch } from "react-redux";
+
+import Colors from "../../constants/Colors";
+import ImagePicker from "../../components/ImagePicker";
 
 const ReportScreen = props => {
+  const [titleValue, setTitleValue] = useState("");
+  const [selectedImage, setSelectedImage] = useState();
+
+  const titleChangeHandler = text => {
+    // you could add validation
+    setTitleValue(text);
+  };
+  const imageTakenHandler = imagePath => {
+    setSelectedImage(imagePath);
+  };
+
+  const saveReportHandler = () => {
+    console.log(selectedImage);
+    console.log(titleValue);
+  };
+
   return (
-    <View>
-      <Text>ReportScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <ImagePicker onImageTaken={imageTakenHandler} />
+        <Button
+          title="Save Report"
+          color={Colors.primary}
+          onPress={saveReportHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
+ReportScreen.navigationOptions = {
+  headerTitle: "Add Report"
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
   }
 });
 
