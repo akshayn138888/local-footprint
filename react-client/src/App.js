502c5e256a7c1a3112808065d0369fd1131cbe96
Akshay Nanda <akshayn@sfu.ca>
2020-06-04 16:42:30 -0700

    Able to print images realtime with firebase storage

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("https://location-app-5d3d8.firebaseio.com/images.json", {
+        method: "GET"
+      })
+        .then(e => e.json())
+        .then(data => {
+          console.log(data);
+          setImages(data);
+        });
+    }, 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  if (images) {
+    let parseData = [];
+    for (let [key, value] of Object.entries(images)) {
+      //console.log(value);
+      for (let [key1, value1] of Object.entries(value)) {
+        parseData.push(
+          <img src={value1.url} alt="red" style={{ width: 200, height: 200 }} />
+        );
+      }
+    }
+    console.log(parseData);
+
+    return (
+      <div className="App">
+        {parseData ? parseData.map(image => image) : ""}
+        <h1>Hi there!</h1>
+      </div>
+    );
+  } else {
+    return <div className="App">Loading</div>;
+  }
 }
 
 export default App;
