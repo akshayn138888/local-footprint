fbc13ae9535669fdf534349df3a6a03846fcdf96
Akshay Nanda <akshayn@sfu.ca>
2020-06-05 14:13:33 -0700

    User Authentication for admin

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import DisplayReport from './Screens/DisplayReport'
-import MapScreen from './Screens/MapScreen'
+import DisplayReport from "./Screens/DisplayReport";
+import MapScreen from "./Screens/MapScreen";
+import Auth from "./Screens/Auth";
 import "./App.css";
 
 function App() {
-
   return (
     <div>
       {/* <DisplayReport /> */}
-      <MapScreen />
+      {/* <MapScreen /> */}
+      <Auth />
     </div>
   );
 }
