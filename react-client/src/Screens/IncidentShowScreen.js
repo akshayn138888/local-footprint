2b059ded47424893bc8d4cfde2558b208ba3a3bb
Akshay Nanda <akshayn@sfu.ca>
2020-06-06 23:59:44 -0700

    added map view to the location screen

--- react-client/src/Screens/IncidentShowScreen.js
+++ react-client/src/Screens/IncidentShowScreen.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Box, Grid, Image, Text, CheckBox } from "grommet";
+import { Box, Grid, Image, Text, CheckBox, Calendar } from "grommet";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 
 const IncidentShowScreen = props => {
   const [report, setReport] = useState(null);
 
   const { userId, reportId } = props.match.params;
+
+  const [viewport, setViewport] = useState({
+    latitude: 47.119706917599885,
+    longitude: -124.112312312,
+    zoom: 15,
+    width: "100vw",
+    height: "100vh"
+  });
 
   useEffect(() => {
     fetch(
@@ -24,7 +33,7 @@ const IncidentShowScreen = props => {
       <>
         <Box margin={{ horizontal: "5%" }}>
           <Grid
-            rows={["xsmall", "xsmall", "medium", "large", "large", "small"]}
+            rows={["xsmall", "xsmall", "medium", "medium", "medium", "small"]}
             columns={["70%", "30%"]}
             areas={[
               ["header", "header"],
@@ -66,20 +75,56 @@ const IncidentShowScreen = props => {
                 style={{
                   color: "gainsboro",
                   backgroundColor: "gainsboro",
-                  height: 1.0
+                  height: 0.8
                 }}
               ></div>
               <h3> Details </h3>
               <Text>{report.description}</Text>
             </Box>
 
-            <Box background="dark-2" gridArea="chat" round={true}>
-              Chat
+            <Box gridArea="chat" align="center" margin={{ top: "10%" }}>
+              <Calendar
+                size="small"
+                date={new Date().toISOString()}
+                onSelect={date => {}}
+                fit="contain"
+              />
+              {/* <Card
+                img="https://picsum.photos/id/54/400/300"
+                title="What I learned from my visit to The Upside Down"
+                author="Nancy Wheeler"
+              /> */}
             </Box>
 
             {/* ROW 5 */}
-            <Box background="dark-2" gridArea="location">
-              Location
+            <Box
+              background="dark-2"
+              gridArea="location"
+              overflow="hidden"
+              round={true}
+              fit="cover"
+            >
+              <div>
+                <ReactMapGL
+                  {...viewport}
+                  mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+                  mapStyle="mapbox://styles/akkin13/ckb1qv50i0grp1inr5bx1qkfm"
+                  onViewportChange={viewport => setViewport(viewport)}
+                >
+                  <Marker
+                    latitude={parseFloat(report.latitude)}
+                    longitude={parseFloat(report.longitude)}
+                  >
+                    <button class="marker-btn">
+                      <img
+                        src="https://cdn.imgbin.com/12/4/19/imgbin-auxiliary-police-lawyer-material-people-s-police-UcqivXX1JKh98eQ2xMj1Zyan2.jpg"
+                        alt="security guard"
+                        style={{ width: 50 }}
+                      />
+                    </button>
+                  </Marker>
+                </ReactMapGL>
+              </div>
             </Box>
 
             {/* ROW 6 */}
